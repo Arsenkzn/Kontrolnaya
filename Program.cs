53412f2e2541898bacca36748dce4c4625d6252d
«Arsen» <rchi1504@gmail.com>
2022-06-23 00:00:51 +0300

    created program

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿String[] arr = { "hello", "27", "word", ":-)", "ref", "65f" };
+string Print(string[] arr)
+{
+    string res = string.Empty;
+    res = "[";
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (Convert.ToString(arr[i]).Length <= 3)
+        {
+            res += $"\"{arr[i]}\"";
+            if (i < arr.Length - 1)
+                res += ", ";
+        }
+    }
+    res += "]";
+    return res;
+}
+Console.WriteLine(Print(arr));
